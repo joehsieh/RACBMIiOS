1bb0528d9d1d5ebd1e0880a1b3d1b120f07b8cb3
joehsieh <ninja31312@gmail.com>
2015-08-29 21:57:14 +0800

    Tunes code for JHImperativeParadigmBMIViewController.

--- BMIiOS/JHImperativeParadigmBMIViewController.m
+++ BMIiOS/JHImperativeParadigmBMIViewController.m
@@ -40,6 +40,7 @@
     NSNumber *BMI = @(kiloOfWeight / (meterOfHeight * meterOfHeight));
     self.BMIValueLabel.text = [BMI stringValue];
     [self jh_updateBMIStatusWithBMIValue:self.BMIValueLabel.text];
+    [self jh_updateBackgroundColorWithBMIValue:self.BMIValueLabel.text];
 }
 
 - (void)jh_updateBMIStatusWithBMIValue:(NSString *)inString
@@ -49,34 +50,47 @@
         return;
     }
     CGFloat BMI = [inString floatValue];
+    self.BMIStatusLabel.text = [self jh_BMIDictionaryWith:BMI][kMessageKey];
+}
+
+- (void)jh_updateBackgroundColorWithBMIValue:(NSString *)inString
+{
+    if ([inString isEqualToString:@"Invalid input"]) {
+        self.BMIStatusLabel.text = inString;
+        return;
+    }
+    CGFloat BMI = [inString floatValue];
+    self.view.backgroundColor = [self jh_BMIDictionaryWith:BMI][kColorKey];
+}
+
+- (NSDictionary *)jh_BMIDictionaryWith:(CGFloat)inBMIValue
+{
     NSString *result = @"";
     UIColor *color = [UIColor whiteColor];
-    if (BMI < kBMINormalWeightLowerBound) {
+    if (inBMIValue < kBMINormalWeightLowerBound) {
         result = @"Under Weight";
         color = [UIColor grayColor];
     }
-    else if (BMI >= kBMINormalWeightLowerBound && BMI < kBMINormalWeightUpperBound) {
+    else if (inBMIValue >= kBMINormalWeightLowerBound && inBMIValue < kBMINormalWeightUpperBound) {
         result = @"Normal Weight";
         color = [UIColor greenColor];
     }
-    else if (BMI >= kBMINormalWeightUpperBound && BMI < kBMIOverWeightUpperBound) {
+    else if (inBMIValue >= kBMINormalWeightUpperBound && inBMIValue < kBMIOverWeightUpperBound) {
         result = @"Over Weight";
         color = [UIColor yellowColor];
     }
-    else if (BMI >= kBMIOverWeightUpperBound && BMI < kBMIClassOneObesityUpperBound) {
+    else if (inBMIValue >= kBMIOverWeightUpperBound && inBMIValue < kBMIClassOneObesityUpperBound) {
         result = @"Class I Weight";
         color = [UIColor lightCoral];
     }
-    else if (BMI >= kBMIClassOneObesityUpperBound && BMI < kBMIClassTwoObesityUpperBound) {
+    else if (inBMIValue >= kBMIClassOneObesityUpperBound && inBMIValue < kBMIClassTwoObesityUpperBound) {
         result = @"Class II Weight";
         color = [UIColor indianRed];
     }
-    else if (BMI >= kBMIClassTwoObesityUpperBound) {
+    else if (inBMIValue >= kBMIClassTwoObesityUpperBound) {
         result = @"Class III Weight";
         color = [UIColor redColor];
     }
-    self.BMIStatusLabel.text = @"Under Weight";
-    self.view.backgroundColor = color;
+    return @{kMessageKey:result, kColorKey: color};
 }
-
 @end
